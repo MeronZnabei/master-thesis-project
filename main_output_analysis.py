6d1542bed45655a78a746a90ea3a5940e99b534d
MeronZnabei <meronznabei@hotmail.com>
2023-12-08 15:21:07 +0100

    cleaning for git

--- main_output_analysis.py
+++ main_output_analysis.py
@@ -6,14 +6,13 @@ if __name__ == '__main__':
 
     DEBUG =True
     if DEBUG:
-        description = "principles"
+        experiment_name = "principles"
         experiments = ["nfe50000_uwf_001_demand", "nfe50000_pwf_100_demand", "nfe50000_gini_01_demand"]     # delete/add an epsilon when switched from None to a principle
         n_seeds = 5
     else:
         # Access the environment variables for input parameters
         experiment_name = os.environ.get("experiment_name")
-        experiments = [os.environ.get("experiments")]
-        n_seeds = int(os.environ.get("n_seeds"))
+        experiments = [str(epsilon) for epsilon in os.environ.get("experiments").split()]
 
     # calculate convergence metrics for the experiments
-    convergence.run(description, experiments, n_seeds)
+    convergence.run(experiment_name, experiments, n_seeds)
